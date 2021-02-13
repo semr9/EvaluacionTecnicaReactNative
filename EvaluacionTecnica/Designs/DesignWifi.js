652b887c4125851671a02980c59153ee1219b98a
sebas <semr9>
2021-02-13 02:02:10 -0500

    PR:Wifi Implamentation

---
+++ EvaluacionTecnica/Designs/DesignWifi.js
@@ -0,0 +1,59 @@
+import {
+    StyleSheet,
+  } from 'react-native';
+import { color } from 'react-native-reanimated';
+  
+const styles = StyleSheet.create({
+    container: {
+      flex: 1, 
+      backgroundColor: '#009387'
+    },
+    header: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        paddingHorizontal: 20,
+        paddingBottom: 50
+    },
+    footer: {
+        flex: 3,
+        backgroundColor: '#fff',
+        borderTopLeftRadius: 30,
+        borderTopRightRadius: 30,
+        paddingHorizontal: 20,
+        paddingVertical: 30
+    },
+    text_header: {
+        color: '#fff',
+        fontWeight: 'bold',
+        fontSize: 30
+    },
+    text_footer: {
+        color: '#05375a',
+        fontSize: 18
+    },
+    action: {
+        flexDirection: 'row',
+        marginTop: 10,
+        borderBottomWidth: 1,
+        borderBottomColor: '#f2f2f2',
+        paddingBottom: 5
+    },
+    textInput: {
+        flex: 1,
+        marginTop: Platform.OS === 'ios' ? 0 : -12,
+        paddingLeft: 10,
+        justifyContent:'center',
+
+        color: '#05375a',
+    },
+    button: {
+        alignItems: 'center',
+        marginTop: 50,
+    },
+    content: {
+        justifyContent: 'center',
+        alignItems:'center',
+    }
+  });
+
+  export default styles;
--- EvaluacionTecnica/screens/wifiScreen.js
+++ EvaluacionTecnica/screens/wifiScreen.js
@@ -1,22 +1,85 @@
-import React from 'react';
-import { View} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text , FlatList,SafeAreaView, StyleSheet,StatusBar, Button} from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import { PermissionsAndroid } from 'react-native';
+import WifiManager from "react-native-wifi-reborn";
+import styles from '../Designs/DesignWifi';
 
 
-
-
+const Item = ({ SSID ,BSSID}) => (
+  <View  style={styles.action} >
+    <Text style={styles.textInput}> {SSID} </Text>
+    <Text style={styles.textInput}> {BSSID} </Text>
+  </View>
+);
 
 function WifiScreen({navigation}) {
 
- 
+  const [dataWifi, setDataWifi] = useState([]);
+
+  const scanExample = async () => {
+    try {
+      const data = await WifiManager.reScanAndLoadWifiList()
+      setDataWifi(data);
+      console.log(dataWifi);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const requestLocationPermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: "React Native Wifi Reborn App Permission",
+          message:
+            "Location permission is required to connect with or scan for Wifi networks. ",
+          buttonNeutral: "Ask Me Later",
+          buttonNegative: "Cancel",
+          buttonPositive: "OK"
+        }
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        scanExample();
+      } else {
+        console.log("Location permission denied");
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+  
+  useEffect(() => {
+    requestLocationPermission();
+  }, []);
+
+
+
+  const renderItem = ({ item }) => (
+    <Item SSID={item.SSID} BSSID={item.BSSID} />
+  );
   
   return (
-    
-      <View >
-       
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+       <Text style={styles.text_header} >See new conncetions</Text> 
       </View>
-  
+      <View style={styles.footer}>
+        <View style={styles.action}>
+         <Text style={styles.text_footer} >Connections</Text>
+        </View>
+        <FlatList
+          data={dataWifi}
+          renderItem={renderItem}
+          keyExtractor={item => item.BSSID}
+        />
+        <View>
+          <Button color='#000000' title="Update" onPress= { () => scanExample()  }/>
+        </View>
+      </View>
+     </SafeAreaView>
       
     
   );
@@ -44,5 +107,9 @@ const stackWifiScreen = ({navigation}) => (
 
   </wifiStack.Navigator>
 );
+
+
+
+
 
 export default stackWifiScreen;
