73e11d94d8e35b40eca6bcb4dc34eabe2dbf4a98
sebas <semr9>
2021-02-13 01:49:15 -0500

    PR:Login Implementation

--- EvaluacionTecnica/screens/loginScreen.js
+++ EvaluacionTecnica/screens/loginScreen.js
@@ -1,14 +1,92 @@
 import * as React from 'react';
-import { View } from 'react-native';
+import styles from '../Designs/DesignLogin'
+import { View, Text, StatusBar,TextInput ,TouchableOpacity } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Feather from 'react-native-vector-icons/Feather';
+import { useTheme } from 'react-native-paper';
 
 function LoginScreen({navigation}) {
+  const { colors } = useTheme();
+  const [data, setData] = React.useState({
+    secureTextEntry: true,
+});
 
+const updateSecureTextEntry = () => {
+  setData({
+      ...data,
+      secureTextEntry: !data.secureTextEntry
+  });
+}
     return (
-      <View>
-        
-        </View> 
+      <View style={styles.container}>
+          <StatusBar backgroundColor='#009387' barStyle="light-content"/>
+        <View style={styles.header}>
+            <Text style={styles.text_header}> Login, please</Text>
+        </View>
+        <View 
+            style={[styles.footer, {
+                backgroundColor: colors.background
+            }]}
+        >
+            <Text style={[styles.text_footer, {
+                color: colors.text
+            }]}>Username</Text>
+            <View style={styles.action}>
+                <FontAwesome 
+                    name="user-o"
+                    color={colors.text}
+                    size={20}
+                />
+                <TextInput 
+                    placeholder="Your Username"
+                    placeholderTextColor="#666666"
+                    style={[styles.textInput, {
+                        color: colors.text
+                    }]}
+                    autoCapitalize="none"
+                />
+            </View>
+            
+            <Text style={[styles.text_footer, {
+                color: colors.text,
+                marginTop: 35
+            }]}>Password</Text>
+            <View style={styles.action}>
+                <Feather 
+                    name="lock"
+                    color={colors.text}
+                    size={20}
+                />
+                <TextInput 
+                    placeholder="Your Password"
+                    placeholderTextColor="#666666"
+                    secureTextEntry={data.secureTextEntry ? true : false}
+                    style={[styles.textInput, {
+                        color: colors.text
+                    }]}
+                    autoCapitalize="none"
+                />
+                <TouchableOpacity
+                    onPress={updateSecureTextEntry}
+                >
+                    {data.secureTextEntry ? 
+                    <Feather 
+                        name="eye-off"
+                        color="grey"
+                        size={20}
+                    />
+                    :
+                    <Feather 
+                        name="eye"
+                        color="grey"
+                        size={20}
+                    />
+                    }
+                </TouchableOpacity>
+            </View>
+        </View>
+       </View> 
     );
   }
  
